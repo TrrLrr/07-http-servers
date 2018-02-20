10cc614ac3424e7234b523444c7f2619e4a1c989
Taylor Stemple <taylorstemple@gmail.com>
2018-02-20 15:07:02 -0800

    finished writeHead for / path

---
+++ lab-taylor/server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const cowsay = require('cowsay');
+const parseBody = require('./lib/body-parse.js');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(function(req,res) {
+  req.url = url.parse(req.url);
+  req.url.query = querystring.parse(req.url.query);
+
+  
+  if(req.method === 'GET' && req.url.pathname === '/cowsay') {
+    let params = req.url.query;
+    res.write(cowsay.say({text: params.text}));
+    res.end();
+  }
+  
+  if(req.method === 'GET') {
+    res.writeHead(200, 'Hello from the server!', {'Content-Type': 'text/plain'});
+    res.end();
+  }
+  
+
+  if(req.method === 'POST' || req.method === 'PUT') {
+    parseBody(req, function(err) {
+      if(err) throw new Error('error parsing body');
+      console.log('request body:', req.body);
+    });
+    
+  }
+  
+});
+
+server.listen(PORT, () => {
+  console.log(`server up: ${PORT}`);
+});
