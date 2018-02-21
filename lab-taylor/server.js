25fc782accd02c0f1df9a5c162e07a49fd499df2
Taylor Stemple <taylorstemple@gmail.com>
2018-02-20 18:32:30 -0800

    finally got POST routes working

--- lab-taylor/server.js
+++ lab-taylor/server.js
@@ -7,16 +7,17 @@ const cowsay = require('cowsay');
 const parseBody = require('./lib/body-parse.js');
 const PORT = process.env.PORT || 3000;
 
+
+//server creation and parsing of query string
 const server = http.createServer(function(req,res) {
   req.url = url.parse(req.url);
   req.url.query = querystring.parse(req.url.query);
 
-  
+  //GET method at cowsay route with valid and invalid text entries 
   if(req.method === 'GET' && req.url.pathname === '/cowsay') {
     let params = req.url.query;
     if('text' in params === false) {
-      console.log('no params hit');
-      res.writeHead(400);
+      res.writeHead(400, {'Content-Type': 'text/plain'});
       res.write(cowsay.say({text: 'Bad Request'}));
       res.end();
     }
@@ -26,23 +27,41 @@ const server = http.createServer(function(req,res) {
     res.end();
     }
   }
-  
+      
+  //GET request to the route of the server with standard response message
   if(req.method === 'GET') {
     res.writeHead(200, 'Hello from the server!', {'Content-Type': 'text/plain'});
     res.end();
   }
   
-
-  if(req.method === 'POST' || req.method === 'PUT') {
-    parseBody(req, function(err) {
-      if(err) throw new Error('error parsing body');
-      console.log('request body:', req.body);
-    });
+  //POST methods at cowsay route with valid and invalid text entries 
+  if(req.method === 'POST' && req.url.pathname === '/cowsay') {
     
+    parseBody(req, function() {
+      
+      
+      if(req.body.text) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(cowsay.say({text: req.body.text}));
+        res.end();
+      }
+      
+      if(!req.body.text) {
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.write(cowsay.say({text: 'Bad Request'}));
+        res.end();
+      }
+      
+    });
   }
-  
 });
-
+      
 server.listen(PORT, () => {
   console.log(`server up: ${PORT}`);
 });
+      
+      
+    
+
+    
+
